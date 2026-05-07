2bbddd2cb7183444734df95607df41306066833d
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 01:12:33 +0200

    Runes data

---
+++ src/data/runes.js
@@ -0,0 +1,32 @@
+export const runes = [
+  {
+    id: 'void-star',
+    name: 'Void Star',
+    symbol: '✦',
+  },
+  {
+    id: 'ashen-eye',
+    name: 'Ashen Eye',
+    symbol: '◉',
+  },
+  {
+    id: 'silent-moon',
+    name: 'Silent Moon',
+    symbol: '☾',
+  },
+  {
+    id: 'broken-sigil',
+    name: 'Broken Sigil',
+    symbol: '◆',
+  },
+  {
+    id: 'dark-flame',
+    name: 'Dark Flame',
+    symbol: '♨',
+  },
+  {
+    id: 'echo-mark',
+    name: 'Echo Mark',
+    symbol: '⌬',
+  },
+]
