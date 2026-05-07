79a97d1eeebb3fc7aed445bd736584b5129ac430
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 01:08:50 +0200

    Rune card

---
+++ src/components/RuneCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  rune: {
+    type: Object,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+defineEmits(['select'])
+</script>
+
+<template>
+  <button class="rune-card" :class="{ 'rune-card--active': active }" :disabled="disabled"
+    @click="$emit('select', rune.id)">
+    <span class="rune-card__symbol">{{ rune.symbol }}</span>
+    <span class="rune-card__name">{{ rune.name }}</span>
+  </button>
+</template>
