b3d13bf80796b2404773dab451dc255c1de727bc
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 22:22:29 +0200

    Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home-view">
+    <section class="hero">
+      <p class="eyebrow">L'Ura Death's Dirge</p>
+
+      <h1>Memorize the runes, invoke the sequence</h1>
+
+      <p class="hero-description">
+        Welcome, Summoner
+        The ancient runes will appear before you. Memorize their order and reproduce the sequence to complete the
+        ritual.
+      </p>
+
+      <RouterLink class="main-button" to="/game">
+        Begin the Ritual
+      </RouterLink>
+    </section>
   </main>
 </template>
