da4aef2e4731919c2fb4a652ebb00a14a78f023b
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 11:05:51 +0200

    Game board

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useGameStore } from '@/stores/gameStore';
+import RuneCard from './RuneCard.vue';
+
+const game = useGameStore()
+</script>
+
+<template>
+  <section class="game-board">
+    <RuneCard v-for="rune in game.availableRunes" :key="rune.id" :rune="rune"
+      :active="game.highlightedRuneId === rune.id" :disabled="!game.canPlay" @select="game.selectRune">
+    </RuneCard>
+  </section>
+</template>
