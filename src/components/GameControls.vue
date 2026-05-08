5c2c78197c76f610beb95d2e737f04a1d2872f27
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 22:08:19 +0200

    Game controls

---
+++ src/components/GameControls.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useGameStore } from '@/stores/gameStore';
+
+const game = useGameStore()
+</script>
+
+<template>
+  <section class="game-controls">
+    <p>{{ game.message }}</p>
+
+    <button class="main-button" @click="game.startGame">
+      {{ game.isPlaying ? 'Retry' : 'Begin the Ritual' }}
+    </button>
+
+    <button v-if="game.isPlaying" class="secondary-button" @click="game.endGame">
+      Give up
+    </button>
+  </section>
+</template>
