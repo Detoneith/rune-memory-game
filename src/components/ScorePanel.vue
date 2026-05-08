3d65b8a4ce20ce7dbdcb77bd9b73b72c58df2cc7
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 21:58:47 +0200

    Score panel

---
+++ src/components/ScorePanel.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useGameStore } from '@/stores/gameStore';
+
+const game = useGameStore()
+</script>
+
+<template>
+  <aside class="score-panel">
+    <div>
+      <span>Level</span>
+      <strong>{{ game.level }}</strong>
+    </div>
+
+    <div>
+      <span>Score</span>
+      <strong>{{ game.score }}</strong>
+    </div>
+
+    <div>
+      <span>Mistakes</span>
+      <strong>{{ game.mistakes }}/3</strong>
+    </div>
+  </aside>
+</template>
