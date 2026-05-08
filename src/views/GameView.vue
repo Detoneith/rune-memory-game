d2ffc06360ae9139284d9c1875ed0a8b8adfcd86
Stoyan Stoyanov <stoyansho@gmail.com>
2026-05-08 22:30:27 +0200

    Game view

---
+++ src/views/GameView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import GameBoard from '@/components/GameBoard.vue';
+import GameControls from '@/components/GameControls.vue';
+import ScorePanel from '@/components/ScorePanel.vue';
+</script>
+
+<template>
+  <main class="game-header">
+    <header class="game-header">
+      <RouterLink to="/" class="back-link">
+        ← Menu
+      </RouterLink>
+      <h1>Chamber of Glyphs</h1>
+    </header>
+
+    <ScorePanel></ScorePanel>
+    <GameControls></GameControls>
+    <GameBoard></GameBoard>
+  </main>
+</template>
